a0757e8871e4e61908ae8fe21b3505b5019e987d
Hollowdaok <89963434+Hollowdaok@users.noreply.github.com>
2022-09-18 17:17:42 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var ts = TimeSpan.FromSeconds(21356);
+
+        Console.WriteLine("{0} ч. {1} хв. {2} с.", ts.Hours, ts.Minutes, ts.Seconds);
+
+        Console.ReadKey();
+    }
+}
