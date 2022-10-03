6b7eef90f19c4f9674fdf9a9c64c4f4813eddc10
Hollowdaok <89963434+Hollowdaok@users.noreply.github.com>
2022-10-03 18:30:41 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введіть час в секундах : ");
+int n = Int32.Parse(Console.ReadLine());
+int hour = n / 3600;
+int min = (n - hour * 3600) / 60;
+int sec = n - hour * 3600 - min * 60;
+Console.WriteLine("\n{0} сек. мають : {1} год. {2} хв. {3} сек.", n, hour, min, sec);
+
+Console.ReadKey();
