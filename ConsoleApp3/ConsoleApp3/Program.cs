a3b7ca1e4d84127511ead3a7991be940ecd23ad5
Hollowdaok <89963434+Hollowdaok@users.noreply.github.com>
2022-10-05 00:55:43 +0300

    Delete ConsoleApp3 directory

--- ConsoleApp3/ConsoleApp3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введіть чотиризначне число: ");
-string str1 = Console.ReadLine();
-char[] ch = str1.ToCharArray();
-Array.Reverse(ch);
-string str2 = new string(ch);
-if (str1 == str2)
-    Console.WriteLine("Дане чотиризначне число читається одинаково зліва направо і навпаки");
-else
-    Console.WriteLine("Дане чотиризначне число не читається одинаково зліва направо і навпаки");
