e07471479f6607509fd95a50d2d503701a01218c
Hollowdaok <89963434+Hollowdaok@users.noreply.github.com>
2022-10-11 17:33:20 +0300

    Delete ConsoleApp3 directory

--- ConsoleApp3/ConsoleApp3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введіть число: ");
-string str1 = Console.ReadLine();
-char[] ch = str1.ToCharArray();
-Array.Reverse(ch);
-string str2 = new string(ch);
-if (str1 == str2)
-    Console.WriteLine("Дане число читається одинаково зліва направо і навпаки(TRUE)");
-else
-    Console.WriteLine("Дане число не читається одинаково зліва направо і навпаки(FALSE)");
