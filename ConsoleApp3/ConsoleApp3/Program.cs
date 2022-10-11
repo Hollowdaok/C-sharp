e69c5615b5130584a6df763e3062f67d3b428cae
Hollowdaok <89963434+Hollowdaok@users.noreply.github.com>
2022-10-11 17:34:11 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = 0;
+do
+{
+    Console.Write("Enter number: ");
+    int.TryParse(Console.ReadLine(), out number);
+} while (number < 1000 || number > 9999);
+
+Console.WriteLine($"Number is {(CheckNumber(number) ? "symmetric(true)" : "not symmetric(false)")}");
+
+bool CheckNumber(int n)
+{
+    int[] digits = new int[4];
+    for (int i = 0; i < digits.Length; i++)
+    {
+        digits[i] = n % 10;
+        n /= 10;
+    }
+    return digits[0] == digits[3] && digits[1] == digits[2];
+}
